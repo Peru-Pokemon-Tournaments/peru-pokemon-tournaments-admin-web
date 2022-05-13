95f162508c59eca074bf64e5e4ae274f5fc1030b
Jibaru <ignacioruedaboada@hotmail.com>
2022-05-12 20:53:56 -0500

    Updated auth view

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Auth</h1>
+  <div id="auth">
+    <base-card>
+      <template #header>
+        <h1>Ingresar</h1>
+      </template>
+      <form>
+        <div class="form-group">
+          <label>Email</label>
+          <base-input type="email" />
+        </div>
+        <div class="form-group">
+          <label>Contraseña</label>
+          <base-input type="password" />
+        </div>
+        <div class="center">
+          <base-button>Iniciar Sesión</base-button>
+        </div>
+      </form>
+    </base-card>
+  </div>
 </template>
+<style lang="scss" scoped>
+@include form-group;
+@include layout;
+
+#auth {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $app-login-background-color;
+}
+</style>
