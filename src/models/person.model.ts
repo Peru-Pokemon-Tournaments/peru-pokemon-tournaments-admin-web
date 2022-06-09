d88efaf7b0fd413fa2f002a3f9b9688cd69e87fd
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-09 14:02:07 -0500

    Added person, competitor and user models

---
+++ src/models/person.model.ts
@@ -0,0 +1,33 @@
+import { Serializable } from "./contracts/serializable";
+
+export class Person implements Serializable {
+  constructor(
+    private _id: string,
+    private _firstName: string,
+    private _lastName: string
+  ) {}
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get firstName(): string {
+    return this._firstName;
+  }
+
+  public get lastName(): string {
+    return this._lastName;
+  }
+
+  public static fromJson(json: any): Person {
+    return new Person(json["id"], json["first_name"], json["last_name"]);
+  }
+
+  public toJson(): object {
+    return {
+      id: this.id,
+      first_name: this.firstName,
+      last_name: this.lastName,
+    };
+  }
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,53 @@
+import { Competitor } from "./competitor.model";
+import { Serializable } from "./contracts/serializable";
+import { Person } from "./person.model";
+
+export class User implements Serializable {
+  constructor(
+    private _id: string,
+    private _name: string,
+    private _email: string,
+    private _person: Person,
+    private _competitor: Competitor
+  ) {}
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+
+  public get person(): Person {
+    return this._person;
+  }
+
+  public get competitor(): Competitor {
+    return this._competitor;
+  }
+
+  public static fromJson(json: any): User {
+    return new User(
+      json["id"],
+      json["name"],
+      json["email"],
+      Person.fromJson(json["person"]),
+      Competitor.fromJson(json["competitor"])
+    );
+  }
+
+  public toJson(): object {
+    return {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+      person: this.person.toJson(),
+      competitor: this.competitor.toJson(),
+    };
+  }
+}
