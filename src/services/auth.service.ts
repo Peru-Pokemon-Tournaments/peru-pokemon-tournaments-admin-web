64e6082f21eeb5f541cb1f9b903d252cad283f41
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-09 15:44:01 -0500

    Created auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { AxiosError, AxiosInstance } from "axios";
+import { ADMIN_LOGIN_URI, API_DOMAIN } from "@/config/services-uri.config";
+import { ResponseError } from "./interfaces/response-error";
+import { User } from "@/models/user.model";
+
+export interface AuthService {
+  login(
+    email: string,
+    password: string
+  ): Promise<{ user: User; token: string; message: string }>;
+}
+
+export class ApiAuthService implements AuthService {
+  constructor(private _httpClient: AxiosInstance) {}
+
+  async login(
+    email: string,
+    password: string
+  ): Promise<{ user: User; token: string; message: string }> {
+    try {
+      const response = await this._httpClient.post(
+        API_DOMAIN + ADMIN_LOGIN_URI,
+        {
+          email,
+          password,
+        }
+      );
+      return {
+        user: User.fromJson(response?.data?.user),
+        token: response?.data?.token as string,
+        message: response?.data?.message as string,
+      };
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+}
