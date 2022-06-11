0a292e72dc539f2ff359ea2df541ed1977e369b6
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 16:27:58 -0500

    Added roles service to pinia custom properties

--- src/shims-pinia.d.ts
+++ src/shims-pinia.d.ts
@@ -1,10 +1,12 @@
 import "pinia";
 import { AuthService } from "./services/auth.service";
 import { PeopleService } from "./services/people.service";
+import { RolesService } from "./services/roles.service";
 
 declare module "pinia" {
   export interface PiniaCustomProperties {
     authService: AuthService;
     peopleService: PeopleService;
+    rolesService: RolesService;
   }
 }
