7aa77e59be390df778086d840156b5b19ef6d0de
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 15:43:35 -0500

    Created paginated response

---
+++ src/services/interfaces/paginated-response.ts
@@ -0,0 +1,31 @@
+import { Serializable } from "@/models/contracts/serializable";
+
+export class PaginatedResponse<T extends Serializable> {
+  constructor(
+    private _data: T[],
+    private _currentPage: number,
+    private _lastPage: number,
+    private _perPage: number,
+    private _totalPages: number
+  ) {}
+
+  public get data(): T[] {
+    return this._data;
+  }
+
+  public get currentPage(): number {
+    return this._currentPage;
+  }
+
+  public get lastPage(): number {
+    return this._lastPage;
+  }
+
+  public get perPage(): number {
+    return this._perPage;
+  }
+
+  public get totalPages(): number {
+    return this._totalPages;
+  }
+}
