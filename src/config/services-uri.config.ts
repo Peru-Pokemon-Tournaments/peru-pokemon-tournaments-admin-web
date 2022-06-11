0f33ef7b940ea6c310f63a3de982ee0bed78e309
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 15:42:31 -0500

    Added fetch people uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -1,3 +1,4 @@
 export const API_DOMAIN =
   process.env.VUE_APP_API_DOMAIN ?? "http://localhost:8000";
 export const ADMIN_LOGIN_URI = "/api/admin/login";
+export const FETCH_PEOPLE = "/api/people";
