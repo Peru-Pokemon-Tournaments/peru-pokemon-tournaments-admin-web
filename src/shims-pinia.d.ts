fb464ee0d519d2f7e6e5b523c911a666ade4dd12
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 15:44:40 -0500

    Added people service in pinia custom properties

--- src/shims-pinia.d.ts
+++ src/shims-pinia.d.ts
@@ -1,8 +1,10 @@
 import "pinia";
 import { AuthService } from "./services/auth.service";
+import { PeopleService } from "./services/people.service";
 
 declare module "pinia" {
   export interface PiniaCustomProperties {
     authService: AuthService;
+    peopleService: PeopleService;
   }
 }
