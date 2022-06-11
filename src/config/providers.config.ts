d545b71f9990b1bfb8d79f78fb030b4aa4020aa6
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 16:59:49 -0500

    Added devices service to app providers

--- src/config/providers.config.ts
+++ src/config/providers.config.ts
@@ -1,15 +1,18 @@
 import { ApiAuthService, AuthService } from "@/services/auth.service";
+import { ApiDevicesService, DevicesService } from "@/services/devices.service";
 import { ApiPeopleService, PeopleService } from "@/services/people.service";
 import { ApiRolesService, RolesService } from "@/services/roles.service";
 import axios, { AxiosInstance } from "axios";
 const httpClient: AxiosInstance = axios.create();
 
 const authService: AuthService = new ApiAuthService(httpClient);
+const devicesService: DevicesService = new ApiDevicesService(httpClient);
 const peopleService: PeopleService = new ApiPeopleService(httpClient);
 const rolesService: RolesService = new ApiRolesService(httpClient);
 
 const ServiceProvider = {
   authService,
+  devicesService,
   peopleService,
   rolesService,
 };
