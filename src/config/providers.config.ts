4e1281be3148ac60fef68936b03e51866f0160a7
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 15:44:11 -0500

    Added people service to app providers

--- src/config/providers.config.ts
+++ src/config/providers.config.ts
@@ -1,11 +1,14 @@
 import { ApiAuthService, AuthService } from "@/services/auth.service";
+import { ApiPeopleService, PeopleService } from "@/services/people.service";
 import axios, { AxiosInstance } from "axios";
 const httpClient: AxiosInstance = axios.create();
 
 const authService: AuthService = new ApiAuthService(httpClient);
+const peopleService: PeopleService = new ApiPeopleService(httpClient);
 
 const ServiceProvider = {
   authService,
+  peopleService,
 };
 
 export default ServiceProvider;
