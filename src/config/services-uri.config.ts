8ff56160ff19b3516d14aae249bd0658ab946b55
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 16:27:01 -0500

    Added fetch roles uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -2,3 +2,4 @@ export const API_DOMAIN =
   process.env.VUE_APP_API_DOMAIN ?? "http://localhost:8000";
 export const ADMIN_LOGIN_URI = "/api/admin/login";
 export const FETCH_PEOPLE = "/api/people";
+export const FETCH_ROLES = "/api/roles";
