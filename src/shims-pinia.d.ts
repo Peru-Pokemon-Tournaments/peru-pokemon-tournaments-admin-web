a176c0f4dd5759cd651134b952da6108db9e064c
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 17:00:09 -0500

    Added devices service to pinia custom properties

--- src/shims-pinia.d.ts
+++ src/shims-pinia.d.ts
@@ -1,11 +1,13 @@
 import "pinia";
 import { AuthService } from "./services/auth.service";
+import { DevicesService } from "./services/devices.service";
 import { PeopleService } from "./services/people.service";
 import { RolesService } from "./services/roles.service";
 
 declare module "pinia" {
   export interface PiniaCustomProperties {
     authService: AuthService;
+    devicesService: DevicesService;
     peopleService: PeopleService;
     rolesService: RolesService;
   }
