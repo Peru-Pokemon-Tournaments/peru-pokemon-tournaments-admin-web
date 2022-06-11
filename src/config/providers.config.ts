30005e186d3a34c5ead69f0e2c515626072dc6ce
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 16:27:40 -0500

    Added roles services to app providers

--- src/config/providers.config.ts
+++ src/config/providers.config.ts
@@ -1,14 +1,17 @@
 import { ApiAuthService, AuthService } from "@/services/auth.service";
 import { ApiPeopleService, PeopleService } from "@/services/people.service";
+import { ApiRolesService, RolesService } from "@/services/roles.service";
 import axios, { AxiosInstance } from "axios";
 const httpClient: AxiosInstance = axios.create();
 
 const authService: AuthService = new ApiAuthService(httpClient);
 const peopleService: PeopleService = new ApiPeopleService(httpClient);
+const rolesService: RolesService = new ApiRolesService(httpClient);
 
 const ServiceProvider = {
   authService,
   peopleService,
+  rolesService,
 };
 
 export default ServiceProvider;
