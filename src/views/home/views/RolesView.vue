a3661373fdec223a7007a73032c9f53a90e1ae92
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 16:28:52 -0500

    Added roles table

--- src/views/home/views/RolesView.vue
+++ src/views/home/views/RolesView.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>Roles View</h1>
+  <base-card>
+    <template #header>
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-8 col-lg-10">
+          <h1>Roles</h1>
+        </div>
+        <base-button
+          type="button"
+          color="success"
+          class="col-12 col-sm-12 col-md-4 col-lg-2"
+        >
+          Nuevo Rol
+        </base-button>
+      </div>
+    </template>
+  </base-card>
+  <base-card>
+    <roles-table />
+  </base-card>
 </template>
+<script lang="ts">
+import RolesTable from "@/components/app/roles/tables/RolesTable.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    RolesTable,
+  },
+});
+</script>
+<style lang="scss" scoped>
+@include form-group;
+@include responsive-grid;
+@include layout;
+h1 {
+  font-size: 1.5rem;
+  font-weight: 300;
+}
+</style>
