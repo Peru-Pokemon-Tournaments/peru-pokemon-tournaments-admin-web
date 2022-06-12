c3c6ee4813c62d76046ffdce5193093ab4eb6f13
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 19:41:51 -0500

    Added fetch games uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -3,5 +3,6 @@ export const API_DOMAIN =
 export const ADMIN_LOGIN_URI = "/api/admin/login";
 export const FETCH_DEVICES = "/api/devices";
 export const FETCH_GAME_GENERATIONS = "/api/game-generations";
+export const FETCH_GAMES = "/api/games";
 export const FETCH_PEOPLE = "/api/people";
 export const FETCH_ROLES = "/api/roles";
