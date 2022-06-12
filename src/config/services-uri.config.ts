75af2109bd4de2bd1fe7c9f2604ef9d4f187aedd
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-12 14:15:16 -0500

    Created fetch tournaments uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -9,3 +9,4 @@ export const FETCH_ROLES = "/api/roles";
 export const FETCH_TOURNAMENT_RULES = "/api/tournament-rules";
 export const FETCH_TOURNAMENT_SYSTEMS = "/api/tournament-systems";
 export const FETCH_TOURNAMENT_TYPES = "/api/tournament-types";
+export const FETCH_TOURNAMENTS = "/api/tournaments";
