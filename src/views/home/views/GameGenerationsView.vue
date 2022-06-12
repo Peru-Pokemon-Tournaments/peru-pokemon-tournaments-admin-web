ecf14c861ddd71c53483cfb08a245f7b62aab05d
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 19:10:24 -0500

    Added game generations table to game generations view

--- src/views/home/views/GameGenerationsView.vue
+++ src/views/home/views/GameGenerationsView.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>Game Generations View</h1>
+  <base-card>
+    <template #header>
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-8 col-lg-10">
+          <h1>Generaciones de Juegos</h1>
+        </div>
+        <base-button
+          type="button"
+          color="success"
+          class="col-12 col-sm-12 col-md-4 col-lg-2"
+        >
+          Nueva Generación de Juego
+        </base-button>
+      </div>
+    </template>
+  </base-card>
+  <base-card>
+    <game-generations-table />
+  </base-card>
 </template>
+<script lang="ts">
+import GameGenerationsTable from "@/components/app/game-generations/tables/GameGenerationsTable.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    GameGenerationsTable,
+  },
+});
+</script>
+<style lang="scss" scoped>
+@include form-group;
+@include responsive-grid;
+@include layout;
+h1 {
+  font-size: 1.5rem;
+  font-weight: 300;
+}
+</style>
