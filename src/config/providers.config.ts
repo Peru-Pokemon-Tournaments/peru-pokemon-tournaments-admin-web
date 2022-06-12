cba63054bb31534fea945be3a6a51fd8f81968ab
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-11 19:08:45 -0500

    Added game generations service to providers

--- src/config/providers.config.ts
+++ src/config/providers.config.ts
@@ -1,5 +1,9 @@
 import { ApiAuthService, AuthService } from "@/services/auth.service";
 import { ApiDevicesService, DevicesService } from "@/services/devices.service";
+import {
+  ApiGameGenerationsService,
+  GameGenerationsService,
+} from "@/services/game-generations.service";
 import { ApiPeopleService, PeopleService } from "@/services/people.service";
 import { ApiRolesService, RolesService } from "@/services/roles.service";
 import axios, { AxiosInstance } from "axios";
@@ -7,12 +11,15 @@ const httpClient: AxiosInstance = axios.create();
 
 const authService: AuthService = new ApiAuthService(httpClient);
 const devicesService: DevicesService = new ApiDevicesService(httpClient);
+const gameGenerationsService: GameGenerationsService =
+  new ApiGameGenerationsService(httpClient);
 const peopleService: PeopleService = new ApiPeopleService(httpClient);
 const rolesService: RolesService = new ApiRolesService(httpClient);
 
 const ServiceProvider = {
   authService,
   devicesService,
+  gameGenerationsService,
   peopleService,
   rolesService,
 };
