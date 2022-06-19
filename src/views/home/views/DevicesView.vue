93015a2a031b4f28875ea640ca3f92b4df2e82ad
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-18 21:05:27 -0500

    Updated device view to manage create device

--- src/views/home/views/DevicesView.vue
+++ src/views/home/views/DevicesView.vue
@@ -9,6 +9,7 @@
           type="button"
           color="success"
           class="col-12 col-sm-12 col-md-4 col-lg-2"
+          @click="toggleCreateModal"
         >
           Nuevo Dispositivo
         </base-button>
@@ -18,14 +19,37 @@
   <base-card>
     <devices-table />
   </base-card>
+  <base-modal
+    title="Nuevo Dispositivo"
+    type="tiny"
+    :open="isCreateModalOpen"
+    @close="toggleCreateModal"
+  >
+    <create-or-edit-device-form @after-submit="closeModal" />
+  </base-modal>
 </template>
 <script lang="ts">
 import DevicesTable from "@/components/app/devices/tables/DevicesTable.vue";
+import CreateOrEditDeviceForm from "@/components/app/devices/forms/CreateOrEditDeviceForm.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
     DevicesTable,
+    CreateOrEditDeviceForm,
+  },
+  data() {
+    return {
+      isCreateModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateModal() {
+      this.isCreateModalOpen = !this.isCreateModalOpen;
+    },
+    closeModal(): void {
+      this.toggleCreateModal();
+    },
   },
 });
 </script>
