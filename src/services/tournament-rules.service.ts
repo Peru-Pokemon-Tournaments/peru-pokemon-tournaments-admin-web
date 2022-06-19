5681bd753f791c80d2540fba8ca390b263052592
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 16:05:24 -0500

    Added create, get and update tournament rule services

--- src/services/tournament-rules.service.ts
+++ src/services/tournament-rules.service.ts
@@ -1,12 +1,17 @@
 import {
   API_DOMAIN,
+  CREATE_TOURNAMENT_RULE,
   FETCH_TOURNAMENT_RULES,
+  GET_TOURNAMENT_RULE,
+  UPDATE_TOURNAMENT_RULE,
 } from "@/config/services-uri.config";
 import { TournamentRule } from "@/models/tournament-rule.model";
 import { TournamentRuleJson } from "@/models/jsons/tournament-rule.json";
 import { AxiosError, AxiosInstance } from "axios";
 import { PaginatedResponse } from "./interfaces/paginated-response";
 import { ResponseError } from "./interfaces/response-error";
+import { BasicResponse } from "./interfaces/basic-response";
+import { ResourcedResponse } from "./interfaces/resourced-response";
 
 export interface TournamentRulesService {
   fetchTournamentRules(
@@ -14,6 +19,19 @@ export interface TournamentRulesService {
     pageSize?: number,
     token?: string
   ): Promise<PaginatedResponse<TournamentRule>>;
+  createTournamentRule(
+    attributes: { name: string; description: string },
+    token: string
+  ): Promise<BasicResponse>;
+  getTournamentRule(
+    tournamentRuleId: string,
+    token: string
+  ): Promise<ResourcedResponse<TournamentRule>>;
+  updateTournamentRule(
+    tournamentRuleId: string,
+    attributes: { name?: string; description?: string },
+    token: string
+  ): Promise<BasicResponse>;
 }
 
 export class ApiTournamentRulesService implements TournamentRulesService {
@@ -52,6 +70,80 @@ export class ApiTournamentRulesService implements TournamentRulesService {
         response.data?.per_page,
         response.data?.total_pages
       );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async createTournamentRule(
+    attributes: { name: string; description: string },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.post(
+        API_DOMAIN + CREATE_TOURNAMENT_RULE,
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async getTournamentRule(
+    tournamentRuleId: string,
+    token: string
+  ): Promise<ResourcedResponse<TournamentRule>> {
+    try {
+      const response = await this._httpClient.get(
+        API_DOMAIN +
+          GET_TOURNAMENT_RULE.replace(":tournamentRuleId", tournamentRuleId),
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new ResourcedResponse(
+        response.data?.message,
+        TournamentRule.fromJson(response.data?.tournament_rule)
+      );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async updateTournamentRule(
+    tournamentRuleId: string,
+    attributes: { name?: string; description?: string },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.patch(
+        API_DOMAIN +
+          UPDATE_TOURNAMENT_RULE.replace(":tournamentRuleId", tournamentRuleId),
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
     } catch (error: any | Error | AxiosError) {
       throw new ResponseError(error.response.data.message, {
         fields: [error.response.data.message],
