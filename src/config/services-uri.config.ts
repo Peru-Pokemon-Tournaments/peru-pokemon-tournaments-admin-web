15556e32a41b2d9ed9f4eb4b7eef18455841fa49
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-18 21:03:13 -0500

    Added create, get and update device uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -12,7 +12,10 @@ export const FETCH_TOURNAMENT_TYPES = "/api/tournament-types";
 export const FETCH_TOURNAMENTS = "/api/tournaments";
 
 export const CREATE_ROLE = "/api/roles";
+export const CREATE_DEVICE = "/api/devices";
 
 export const GET_ROLE = "/api/roles/:roleId";
+export const GET_DEVICE = "/api/devices/:deviceId";
 
 export const UPDATE_ROLE = "/api/roles/:roleId";
+export const UPDATE_DEVICE = "/api/devices/:deviceId";
