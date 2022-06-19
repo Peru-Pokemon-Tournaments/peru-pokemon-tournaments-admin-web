6e3f8c2e45533811017f9aa6f4b485829698dc50
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-18 21:03:33 -0500

    Added create, get and update device service

--- src/services/devices.service.ts
+++ src/services/devices.service.ts
@@ -1,7 +1,15 @@
-import { API_DOMAIN, FETCH_DEVICES } from "@/config/services-uri.config";
+import {
+  API_DOMAIN,
+  CREATE_DEVICE,
+  FETCH_DEVICES,
+  GET_DEVICE,
+  UPDATE_DEVICE,
+} from "@/config/services-uri.config";
 import { Device } from "@/models/device.model";
 import { AxiosError, AxiosInstance } from "axios";
+import { BasicResponse } from "./interfaces/basic-response";
 import { PaginatedResponse } from "./interfaces/paginated-response";
+import { ResourcedResponse } from "./interfaces/resourced-response";
 import { ResponseError } from "./interfaces/response-error";
 
 export interface DevicesService {
@@ -10,6 +18,19 @@ export interface DevicesService {
     pageSize?: number,
     token?: string
   ): Promise<PaginatedResponse<Device>>;
+  createDevice(
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse>;
+  getDevice(
+    deviceId: string,
+    token: string
+  ): Promise<ResourcedResponse<Device>>;
+  updateDevice(
+    deviceId: string,
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse>;
 }
 
 export class ApiDevicesService implements DevicesService {
@@ -45,6 +66,78 @@ export class ApiDevicesService implements DevicesService {
         response.data?.per_page,
         response.data?.total_pages
       );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async createDevice(
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.post(
+        API_DOMAIN + CREATE_DEVICE,
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async getDevice(
+    deviceId: string,
+    token: string
+  ): Promise<ResourcedResponse<Device>> {
+    try {
+      const response = await this._httpClient.get(
+        API_DOMAIN + GET_DEVICE.replace(":deviceId", deviceId),
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new ResourcedResponse(
+        response.data?.message,
+        Device.fromJson(response.data?.device)
+      );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async updateDevice(
+    deviceId: string,
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.patch(
+        API_DOMAIN + UPDATE_DEVICE.replace(":deviceId", deviceId),
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
     } catch (error: any | Error | AxiosError) {
       throw new ResponseError(error.response.data.message, {
         fields: [error.response.data.message],
