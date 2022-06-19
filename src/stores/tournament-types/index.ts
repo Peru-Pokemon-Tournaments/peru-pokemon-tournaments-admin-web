93ce19c6c4630eac89d648276198b9474b532494
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 15:39:41 -0500

    Added crate, select and update selected tournament type and their states

--- src/stores/tournament-types/index.ts
+++ src/stores/tournament-types/index.ts
@@ -11,11 +11,16 @@ export const useTournamentTypesStore = defineStore("useTournamentTypes", {
   state(): Types.TournamentTypesStoreState {
     return {
       tournamentTypes: [] as TournamentType[],
+      selectedTournamentType: null as TournamentType | null,
       totalPages: 1,
       currentPage: 1,
       lastPage: 1,
       perPage: 15,
       loadingTournamentTypes: false,
+      createTournamentTypeLoading: false,
+      updateSelectedTournamentTypeLoading: false,
+      getSelectedTournamentTypeLoading: false,
+      tournamentTypesChanged: false,
     };
   },
   getters: {
@@ -24,6 +29,21 @@ export const useTournamentTypesStore = defineStore("useTournamentTypes", {
     },
     isLoadingTournamentTypes(): boolean {
       return this.loadingTournamentTypes;
+    },
+    isCreateTournamentTypeLoading(): boolean {
+      return this.createTournamentTypeLoading;
+    },
+    isUpdateSelectedTournamentTypeLoading(): boolean {
+      return this.updateSelectedTournamentTypeLoading;
+    },
+    isGetSelectedTournamentTypeLoading(): boolean {
+      return this.getSelectedTournamentTypeLoading;
+    },
+    tournamentTypesHasChanged(): boolean {
+      return this.tournamentTypesChanged;
+    },
+    hasSelectedTournamentType(): boolean {
+      return this.selectedTournamentType != null;
     },
   },
   actions: {
@@ -56,6 +76,84 @@ export const useTournamentTypesStore = defineStore("useTournamentTypes", {
         }
       } finally {
         this.loadingTournamentTypes = false;
+      }
+    },
+    async createTournamentType(attributes: { name: string }): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.createTournamentTypeLoading = true;
+
+      try {
+        const basicResponse =
+          await this.tournamentTypesService.createTournamentType(
+            attributes,
+            userStore.authToken
+          );
+        toast.success(basicResponse.message);
+        this.tournamentTypesChanged = true;
+        setTimeout(() => (this.tournamentTypesChanged = false), 1000);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.createTournamentTypeLoading = false;
+      }
+    },
+    async selectTournamentType(tournamentTypeId: string): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.getSelectedTournamentTypeLoading = true;
+      this.selectedTournamentType = null;
+
+      try {
+        const response = await this.tournamentTypesService.getTournamentType(
+          tournamentTypeId,
+          userStore.authToken
+        );
+
+        this.selectedTournamentType = response.resource;
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.getSelectedTournamentTypeLoading = false;
+      }
+    },
+
+    async updateSelectedTournamentType(attributes: {
+      name: string;
+    }): Promise<void> {
+      if (!this.hasSelectedTournamentType) {
+        return;
+      }
+
+      const userStore = useAuthStore();
+
+      this.updateSelectedTournamentTypeLoading = true;
+
+      try {
+        const response = await this.tournamentTypesService.updateTournamentType(
+          this.selectedTournamentType!.id,
+          attributes,
+          userStore.authToken
+        );
+        this.tournamentTypesChanged = true;
+        setTimeout(() => (this.tournamentTypesChanged = false), 1000);
+        toast.success(response.message);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.updateSelectedTournamentTypeLoading = false;
       }
     },
   },
--- src/stores/tournament-types/types.ts
+++ src/stores/tournament-types/types.ts
@@ -2,9 +2,14 @@ import { TournamentType } from "@/models/tournament-type.model";
 
 export interface TournamentTypesStoreState {
   tournamentTypes: TournamentType[];
+  selectedTournamentType: TournamentType | null;
   totalPages: number;
   currentPage: number;
   lastPage: number;
   perPage: number;
   loadingTournamentTypes: boolean;
+  createTournamentTypeLoading: boolean;
+  updateSelectedTournamentTypeLoading: boolean;
+  getSelectedTournamentTypeLoading: boolean;
+  tournamentTypesChanged: boolean;
 }
