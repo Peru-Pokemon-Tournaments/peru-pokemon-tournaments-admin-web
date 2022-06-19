9c8bae5039ed8879fc21ac9848561f72657d808c
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-18 20:37:41 -0500

    Added create, get and update role service

--- src/services/roles.service.ts
+++ src/services/roles.service.ts
@@ -1,7 +1,15 @@
-import { API_DOMAIN, FETCH_ROLES } from "@/config/services-uri.config";
+import {
+  API_DOMAIN,
+  CREATE_ROLE,
+  FETCH_ROLES,
+  GET_ROLE,
+  UPDATE_ROLE,
+} from "@/config/services-uri.config";
 import { Role } from "@/models/role.model";
 import { AxiosError, AxiosInstance } from "axios";
+import { BasicResponse } from "./interfaces/basic-response";
 import { PaginatedResponse } from "./interfaces/paginated-response";
+import { ResourcedResponse } from "./interfaces/resourced-response";
 import { ResponseError } from "./interfaces/response-error";
 
 export interface RolesService {
@@ -10,6 +18,16 @@ export interface RolesService {
     pageSize?: number,
     token?: string
   ): Promise<PaginatedResponse<Role>>;
+  createRole(
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse>;
+  getRole(roleId: string, token: string): Promise<ResourcedResponse<Role>>;
+  updateRole(
+    roleId: string,
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse>;
 }
 
 export class ApiRolesService implements RolesService {
@@ -45,6 +63,78 @@ export class ApiRolesService implements RolesService {
         response.data?.per_page,
         response.data?.total_pages
       );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async createRole(
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.post(
+        API_DOMAIN + CREATE_ROLE,
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async getRole(
+    roleId: string,
+    token: string
+  ): Promise<ResourcedResponse<Role>> {
+    try {
+      const response = await this._httpClient.get(
+        API_DOMAIN + GET_ROLE.replace(":roleId", roleId),
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new ResourcedResponse(
+        response.data?.message,
+        Role.fromJson(response.data?.role)
+      );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(error.response.data.message, {
+        fields: [error.response.data.message],
+      });
+    }
+  }
+
+  async updateRole(
+    roleId: string,
+    attributes: { name: string },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.patch(
+        API_DOMAIN + UPDATE_ROLE.replace(":roleId", roleId),
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
     } catch (error: any | Error | AxiosError) {
       throw new ResponseError(error.response.data.message, {
         fields: [error.response.data.message],
