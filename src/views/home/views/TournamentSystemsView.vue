8ac00e2dc0e9c8cfd0aec305526121430321595a
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 15:23:08 -0500

    Update tournament systems view to manage create

--- src/views/home/views/TournamentSystemsView.vue
+++ src/views/home/views/TournamentSystemsView.vue
@@ -9,6 +9,7 @@
           type="button"
           color="success"
           class="col-12 col-sm-12 col-md-4 col-lg-2"
+          @click="toggleCreateModal"
         >
           Nuevo Sistema de Torneo
         </base-button>
@@ -18,14 +19,37 @@
   <base-card>
     <tournament-systems-table />
   </base-card>
+  <base-modal
+    title="Nuevo Sistema de Torneo"
+    type="medium"
+    :open="isCreateModalOpen"
+    @close="toggleCreateModal"
+  >
+    <create-or-edit-tournament-system-form @after-submit="closeModal" />
+  </base-modal>
 </template>
 <script lang="ts">
 import TournamentSystemsTable from "@/components/app/tournament-systems/tables/TournamentSystemsTable.vue";
+import CreateOrEditTournamentSystemForm from "@/components/app/tournament-systems/forms/CreateOrEditTournamentSystemForm.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
     TournamentSystemsTable,
+    CreateOrEditTournamentSystemForm,
+  },
+  data() {
+    return {
+      isCreateModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateModal() {
+      this.isCreateModalOpen = !this.isCreateModalOpen;
+    },
+    closeModal(): void {
+      this.toggleCreateModal();
+    },
   },
 });
 </script>
