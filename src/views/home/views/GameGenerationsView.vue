b45acc0a6801dc2376a50326a13295c78ede4bfa
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 18:39:59 -0500

    Updated to manage create game generation

--- src/views/home/views/GameGenerationsView.vue
+++ src/views/home/views/GameGenerationsView.vue
@@ -9,6 +9,7 @@
           type="button"
           color="success"
           class="col-12 col-sm-12 col-md-4 col-lg-2"
+          @click="toggleCreateModal"
         >
           Nueva Generación de Juego
         </base-button>
@@ -18,14 +19,37 @@
   <base-card>
     <game-generations-table />
   </base-card>
+  <base-modal
+    title="Nueva Generación de Juego"
+    type="small"
+    :open="isCreateModalOpen"
+    @close="toggleCreateModal"
+  >
+    <create-or-edit-game-generation-form @after-submit="closeModal" />
+  </base-modal>
 </template>
 <script lang="ts">
 import GameGenerationsTable from "@/components/app/game-generations/tables/GameGenerationsTable.vue";
+import CreateOrEditGameGenerationForm from "@/components/app/game-generations/forms/CreateOrEditGameGenerationForm.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
     GameGenerationsTable,
+    CreateOrEditGameGenerationForm,
+  },
+  data() {
+    return {
+      isCreateModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateModal() {
+      this.isCreateModalOpen = !this.isCreateModalOpen;
+    },
+    closeModal(): void {
+      this.toggleCreateModal();
+    },
   },
 });
 </script>
