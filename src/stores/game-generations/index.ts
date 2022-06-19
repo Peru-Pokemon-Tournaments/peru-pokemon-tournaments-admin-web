183401ca9823d2be91e2e7c6cf421f3c964a0cb4
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 18:39:08 -0500

    Added create, select and update selected game generation actions and their states

--- src/stores/game-generations/index.ts
+++ src/stores/game-generations/index.ts
@@ -11,11 +11,16 @@ export const useGameGenerationsStore = defineStore("useGameGenerations", {
   state(): Types.GameGenerationsStoreState {
     return {
       gameGenerations: [] as GameGeneration[],
+      selectedGameGeneration: null as GameGeneration | null,
       totalPages: 1,
       currentPage: 1,
       lastPage: 1,
       perPage: 15,
       loadingGameGenerations: false,
+      createGameGenerationLoading: false,
+      updateSelectedGameGenerationLoading: false,
+      getSelectedGameGenerationLoading: false,
+      gameGenerationsChanged: false,
     };
   },
   getters: {
@@ -24,6 +29,21 @@ export const useGameGenerationsStore = defineStore("useGameGenerations", {
     },
     isLoadingGameGenerations(): boolean {
       return this.loadingGameGenerations;
+    },
+    isCreateGameGenerationLoading(): boolean {
+      return this.createGameGenerationLoading;
+    },
+    isUpdateSelectedGameGenerationLoading(): boolean {
+      return this.updateSelectedGameGenerationLoading;
+    },
+    isGetSelectedGameGenerationLoading(): boolean {
+      return this.getSelectedGameGenerationLoading;
+    },
+    gameGenerationsHasChanged(): boolean {
+      return this.gameGenerationsChanged;
+    },
+    hasSelectedGameGeneration(): boolean {
+      return this.selectedGameGeneration != null;
     },
   },
   actions: {
@@ -56,6 +76,88 @@ export const useGameGenerationsStore = defineStore("useGameGenerations", {
         }
       } finally {
         this.loadingGameGenerations = false;
+      }
+    },
+    async createGameGeneration(attributes: {
+      generation: string;
+      description: string;
+    }): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.createGameGenerationLoading = true;
+
+      try {
+        const basicResponse =
+          await this.gameGenerationsService.createGameGeneration(
+            attributes,
+            userStore.authToken
+          );
+        toast.success(basicResponse.message);
+        this.gameGenerationsChanged = true;
+        setTimeout(() => (this.gameGenerationsChanged = false), 1000);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.createGameGenerationLoading = false;
+      }
+    },
+    async selectGameGeneration(gameGenerationId: string): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.getSelectedGameGenerationLoading = true;
+      this.selectedGameGeneration = null;
+
+      try {
+        const response = await this.gameGenerationsService.getGameGeneration(
+          gameGenerationId,
+          userStore.authToken
+        );
+
+        this.selectedGameGeneration = response.resource;
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.getSelectedGameGenerationLoading = false;
+      }
+    },
+
+    async updateSelectedGameGeneration(attributes: {
+      generation?: string;
+      description?: string;
+    }): Promise<void> {
+      if (!this.hasSelectedGameGeneration) {
+        return;
+      }
+
+      const userStore = useAuthStore();
+
+      this.updateSelectedGameGenerationLoading = true;
+
+      try {
+        const response = await this.gameGenerationsService.updateGameGeneration(
+          this.selectedGameGeneration!.id,
+          attributes,
+          userStore.authToken
+        );
+        this.gameGenerationsChanged = true;
+        setTimeout(() => (this.gameGenerationsChanged = false), 1000);
+        toast.success(response.message);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.updateSelectedGameGenerationLoading = false;
       }
     },
   },
--- src/stores/game-generations/types.ts
+++ src/stores/game-generations/types.ts
@@ -2,9 +2,14 @@ import { GameGeneration } from "@/models/game-generation.model";
 
 export interface GameGenerationsStoreState {
   gameGenerations: GameGeneration[];
+  selectedGameGeneration: GameGeneration | null;
   totalPages: number;
   currentPage: number;
   lastPage: number;
   perPage: number;
   loadingGameGenerations: boolean;
+  createGameGenerationLoading: boolean;
+  updateSelectedGameGenerationLoading: boolean;
+  getSelectedGameGenerationLoading: boolean;
+  gameGenerationsChanged: boolean;
 }
