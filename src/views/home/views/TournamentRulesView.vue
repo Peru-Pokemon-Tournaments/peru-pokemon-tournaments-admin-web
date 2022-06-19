40aeea429fa08a80b31949daafe51f791b5a97ec
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 16:14:03 -0500

    Updated to manage create tournament rule

--- src/views/home/views/TournamentRulesView.vue
+++ src/views/home/views/TournamentRulesView.vue
@@ -9,6 +9,7 @@
           type="button"
           color="success"
           class="col-12 col-sm-12 col-md-4 col-lg-2"
+          @click="toggleCreateModal"
         >
           Nueva Regla de Torneo
         </base-button>
@@ -18,14 +19,37 @@
   <base-card>
     <tournament-rules-table />
   </base-card>
+  <base-modal
+    title="Nueva Regla de Torneo"
+    type="medium"
+    :open="isCreateModalOpen"
+    @close="toggleCreateModal"
+  >
+    <create-or-edit-tournament-rule-form @after-submit="closeModal" />
+  </base-modal>
 </template>
 <script lang="ts">
 import TournamentRulesTable from "@/components/app/tournament-rules/tables/TournamentRulesTable.vue";
+import CreateOrEditTournamentRuleForm from "@/components/app/tournament-rules/forms/CreateOrEditTournamentRuleForm.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
     TournamentRulesTable,
+    CreateOrEditTournamentRuleForm,
+  },
+  data() {
+    return {
+      isCreateModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateModal() {
+      this.isCreateModalOpen = !this.isCreateModalOpen;
+    },
+    closeModal(): void {
+      this.toggleCreateModal();
+    },
   },
 });
 </script>
