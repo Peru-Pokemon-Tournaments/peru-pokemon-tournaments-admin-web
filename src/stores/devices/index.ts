46a63bb04902bfff2542f80912c85064ebf51c5f
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-18 21:04:01 -0500

    Added get, select and update selected device actions and their states

--- src/stores/devices/index.ts
+++ src/stores/devices/index.ts
@@ -11,11 +11,16 @@ export const useDevicesStore = defineStore("useDevices", {
   state(): Types.DevicesStoreState {
     return {
       devices: [] as Device[],
+      selectedDevice: null as Device | null,
       totalPages: 1,
       currentPage: 1,
       lastPage: 1,
       perPage: 15,
       loadingDevices: false,
+      createDeviceLoading: false,
+      updateSelectedDeviceLoading: false,
+      getSelectedDeviceLoading: false,
+      devicesChanged: false,
     };
   },
   getters: {
@@ -24,6 +29,21 @@ export const useDevicesStore = defineStore("useDevices", {
     },
     isLoadingDevices(): boolean {
       return this.loadingDevices;
+    },
+    isCreateDeviceLoading(): boolean {
+      return this.createDeviceLoading;
+    },
+    isUpdateSelectedDeviceLoading(): boolean {
+      return this.updateSelectedDeviceLoading;
+    },
+    isGetSelectedDeviceLoading(): boolean {
+      return this.getSelectedDeviceLoading;
+    },
+    devicesHasChanged(): boolean {
+      return this.devicesChanged;
+    },
+    hasSelectedDevice(): boolean {
+      return this.selectedDevice != null;
     },
   },
   actions: {
@@ -52,6 +72,81 @@ export const useDevicesStore = defineStore("useDevices", {
         }
       } finally {
         this.loadingDevices = false;
+      }
+    },
+    async createDevice({ name }: { name: string }): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.createDeviceLoading = true;
+
+      try {
+        const basicResponse = await this.devicesService.createDevice(
+          { name },
+          userStore.authToken
+        );
+        toast.success(basicResponse.message);
+        this.devicesChanged = true;
+        setTimeout(() => (this.devicesChanged = false), 1000);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.createDeviceLoading = false;
+      }
+    },
+    async selectDevice(deviceId: string): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.getSelectedDeviceLoading = true;
+      this.selectedDevice = null;
+
+      try {
+        const response = await this.devicesService.getDevice(
+          deviceId,
+          userStore.authToken
+        );
+
+        this.selectedDevice = response.resource;
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.getSelectedDeviceLoading = false;
+      }
+    },
+
+    async updateSelectedDevice(attributes: { name: string }): Promise<void> {
+      if (!this.hasSelectedDevice) {
+        return;
+      }
+
+      const userStore = useAuthStore();
+
+      this.updateSelectedDeviceLoading = true;
+
+      try {
+        const response = await this.devicesService.updateDevice(
+          this.selectedDevice!.id,
+          attributes,
+          userStore.authToken
+        );
+        this.devicesChanged = true;
+        setTimeout(() => (this.devicesChanged = false), 1000);
+        toast.success(response.message);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.updateSelectedDeviceLoading = false;
       }
     },
   },
--- src/stores/devices/types.ts
+++ src/stores/devices/types.ts
@@ -2,9 +2,14 @@ import { Device } from "@/models/device.model";
 
 export interface DevicesStoreState {
   devices: Device[];
+  selectedDevice: Device | null;
   totalPages: number;
   currentPage: number;
   lastPage: number;
   perPage: number;
   loadingDevices: boolean;
+  createDeviceLoading: boolean;
+  updateSelectedDeviceLoading: boolean;
+  getSelectedDeviceLoading: boolean;
+  devicesChanged: boolean;
 }
