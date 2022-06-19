8110c7dd94731c9dda2b823b5531f9e097565a49
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-18 20:41:39 -0500

    Updated roles view to manage create role

--- src/views/home/views/RolesView.vue
+++ src/views/home/views/RolesView.vue
@@ -9,6 +9,7 @@
           type="button"
           color="success"
           class="col-12 col-sm-12 col-md-4 col-lg-2"
+          @click="toggleCreateModal"
         >
           Nuevo Rol
         </base-button>
@@ -18,14 +19,37 @@
   <base-card>
     <roles-table />
   </base-card>
+  <base-modal
+    title="Nuevo Rol"
+    type="tiny"
+    :open="isCreateModalOpen"
+    @close="toggleCreateModal"
+  >
+    <create-or-edit-role-form @after-submit="closeModal" />
+  </base-modal>
 </template>
 <script lang="ts">
 import RolesTable from "@/components/app/roles/tables/RolesTable.vue";
+import CreateOrEditRoleForm from "@/components/app/roles/forms/CreateOrEditRoleForm.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
     RolesTable,
+    CreateOrEditRoleForm,
+  },
+  data() {
+    return {
+      isCreateModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateModal() {
+      this.isCreateModalOpen = !this.isCreateModalOpen;
+    },
+    closeModal(): void {
+      this.toggleCreateModal();
+    },
   },
 });
 </script>
