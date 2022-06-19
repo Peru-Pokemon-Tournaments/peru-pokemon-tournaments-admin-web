583aab1200b084a370219456d4196cad221e501e
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 18:38:10 -0500

    Added create, get and update game generation uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -11,12 +11,14 @@ export const FETCH_TOURNAMENT_SYSTEMS = "/api/tournament-systems";
 export const FETCH_TOURNAMENT_TYPES = "/api/tournament-types";
 export const FETCH_TOURNAMENTS = "/api/tournaments";
 
+export const CREATE_GAME_GENERATION = "/api/game-generations";
 export const CREATE_ROLE = "/api/roles";
 export const CREATE_DEVICE = "/api/devices";
 export const CREATE_TOURNAMENT_RULE = "/api/tournament-rules";
 export const CREATE_TOURNAMENT_SYSTEM = "/api/tournament-systems";
 export const CREATE_TOURNAMENT_TYPE = "/api/tournament-types";
 
+export const GET_GAME_GENERATION = "/api/game-generations/:gameGenerationId";
 export const GET_ROLE = "/api/roles/:roleId";
 export const GET_DEVICE = "/api/devices/:deviceId";
 export const GET_TOURNAMENT_RULE = "/api/tournament-rules/:tournamentRuleId";
@@ -24,6 +26,7 @@ export const GET_TOURNAMENT_SYSTEM =
   "/api/tournament-systems/:tournamentSystemId";
 export const GET_TOURNAMENT_TYPE = "/api/tournament-types/:tournamentTypeId";
 
+export const UPDATE_GAME_GENERATION = "/api/game-generations/:gameGenerationId";
 export const UPDATE_ROLE = "/api/roles/:roleId";
 export const UPDATE_DEVICE = "/api/devices/:deviceId";
 export const UPDATE_TOURNAMENT_RULE = "/api/tournament-rules/:tournamentRuleId";
