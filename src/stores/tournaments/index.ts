b4c2881347b3160c1d80bc34478b0303ee9ad157
Jibaru <ignacioruedaboada@hotmail.com>
2022-07-02 17:55:15 -0500

    Updated tournament store

--- src/stores/tournaments/index.ts
+++ src/stores/tournaments/index.ts
@@ -4,6 +4,7 @@ import { useAuthStore } from "../auth";
 import { useToast } from "vue-toastification";
 import * as Types from "./types";
 import { SmallTournament } from "@/models/small-tournament.model";
+import { CreateOrEditTournamentDTO } from "@/components/app/tournaments/forms/interfaces/CreateOrEditTournamentDTO";
 
 const toast = useToast();
 
@@ -11,11 +12,16 @@ export const useTournamentsStore = defineStore("useTournaments", {
   state(): Types.TournamentsStoreState {
     return {
       tournaments: [] as SmallTournament[],
+      selectedTournament: null as SmallTournament | null,
       totalPages: 1,
       currentPage: 1,
       lastPage: 1,
       perPage: 15,
       loadingTournaments: false,
+      createTournamentLoading: false,
+      updateSelectedTournamentLoading: false,
+      getSelectedTournamentLoading: false,
+      tournamentsChanged: false,
     };
   },
   getters: {
@@ -24,6 +30,21 @@ export const useTournamentsStore = defineStore("useTournaments", {
     },
     isLoadingTournaments(): boolean {
       return this.loadingTournaments;
+    },
+    isCreateTournamentLoading(): boolean {
+      return this.createTournamentLoading;
+    },
+    isUpdateSelectedTournamentLoading(): boolean {
+      return this.updateSelectedTournamentLoading;
+    },
+    isGetSelectedTournamentLoading(): boolean {
+      return this.getSelectedTournamentLoading;
+    },
+    tournamentsHasChanged(): boolean {
+      return this.tournamentsChanged;
+    },
+    hasSelectedTournament(): boolean {
+      return this.selectedTournament != null;
     },
   },
   actions: {
@@ -53,6 +74,86 @@ export const useTournamentsStore = defineStore("useTournaments", {
         }
       } finally {
         this.loadingTournaments = false;
+      }
+    },
+    async createTournament(
+      attributes: CreateOrEditTournamentDTO
+    ): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.createTournamentLoading = true;
+
+      attributes.createdByPerson.id = userStore.loggedUser!.person.id;
+
+      try {
+        const basicResponse = await this.tournamentsService.createTournament(
+          attributes,
+          userStore.authToken
+        );
+        toast.success(basicResponse.message);
+        this.tournamentsChanged = true;
+        setTimeout(() => (this.tournamentsChanged = false), 1000);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.createTournamentLoading = false;
+      }
+    },
+    async selectTournament(tournamentId: string): Promise<void> {
+      const userStore = useAuthStore();
+
+      this.getSelectedTournamentLoading = true;
+      this.selectedTournament = null;
+
+      try {
+        const response = await this.tournamentsService.getTournament(
+          tournamentId,
+          userStore.authToken
+        );
+
+        this.selectedTournament = response.resource;
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.getSelectedTournamentLoading = false;
+      }
+    },
+    async updateSelectedTournament(
+      attributes: CreateOrEditTournamentDTO
+    ): Promise<void> {
+      if (!this.hasSelectedTournament) {
+        return;
+      }
+
+      const userStore = useAuthStore();
+
+      this.updateSelectedTournamentLoading = true;
+
+      try {
+        const response = await this.tournamentsService.updateTournament(
+          this.selectedTournament!.id,
+          attributes,
+          userStore.authToken
+        );
+        this.tournamentsChanged = true;
+        setTimeout(() => (this.tournamentsChanged = false), 1000);
+        toast.success(response.message);
+      } catch (error: unknown | ResponseError) {
+        if (error instanceof ResponseError) {
+          toast.error(error.fullErrorMessage);
+        } else {
+          console.warn(error);
+        }
+      } finally {
+        this.updateSelectedTournamentLoading = false;
       }
     },
   },
--- src/stores/tournaments/types.ts
+++ src/stores/tournaments/types.ts
@@ -2,9 +2,14 @@ import { SmallTournament } from "@/models/small-tournament.model";
 
 export interface TournamentsStoreState {
   tournaments: SmallTournament[];
+  selectedTournament: SmallTournament | null;
   totalPages: number;
   currentPage: number;
   lastPage: number;
   perPage: number;
   loadingTournaments: boolean;
+  createTournamentLoading: boolean;
+  updateSelectedTournamentLoading: boolean;
+  getSelectedTournamentLoading: boolean;
+  tournamentsChanged: boolean;
 }
