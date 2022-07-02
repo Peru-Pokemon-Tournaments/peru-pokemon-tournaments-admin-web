a0e33b11e274dc11d0a50df1f9d8a23179c4c188
Jibaru <ignacioruedaboada@hotmail.com>
2022-07-02 17:55:37 -0500

    Created create tournament view

---
+++ src/views/home/views/tournaments/CreateTournamentView.vue
@@ -0,0 +1,13 @@
+<template>
+  <create-or-edit-tournament-form />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import CreateOrEditTournamentForm from "@/components/app/tournaments/forms/CreateOrEditTournamentForm.vue";
+
+export default defineComponent({
+  components: {
+    CreateOrEditTournamentForm,
+  },
+});
+</script>
