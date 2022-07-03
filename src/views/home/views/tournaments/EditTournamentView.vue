7bb0ef996ca1a3967189608ef67ad5a955239091
Jibaru <ignacioruedaboada@hotmail.com>
2022-07-03 16:07:40 -0500

    Created edit tournament view

---
+++ src/views/home/views/tournaments/EditTournamentView.vue
@@ -0,0 +1,19 @@
+<template>
+  <create-or-edit-tournament-form :currentId="tournamentId" />
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import CreateOrEditTournamentForm from "@/components/app/tournaments/forms/CreateOrEditTournamentForm.vue";
+
+export default defineComponent({
+  props: {
+    tournamentId: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  components: {
+    CreateOrEditTournamentForm,
+  },
+});
+</script>
