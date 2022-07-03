05300310111ce4f29ae45cd2fdb28618401f2fba
Jibaru <ignacioruedaboada@hotmail.com>
2022-07-02 22:06:58 -0500

    Added fetch tournament inscriptions uri

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -6,6 +6,7 @@ export const FETCH_GAME_GENERATIONS = "/api/game-generations";
 export const FETCH_GAMES = "/api/games";
 export const FETCH_PEOPLE = "/api/people";
 export const FETCH_ROLES = "/api/roles";
+export const FETCH_TOURNAMENT_INSCRIPTIONS = "/api/inscriptions";
 export const FETCH_TOURNAMENT_RULES = "/api/tournament-rules";
 export const FETCH_TOURNAMENT_SYSTEMS = "/api/tournament-systems";
 export const FETCH_TOURNAMENT_TYPES = "/api/tournament-types";
