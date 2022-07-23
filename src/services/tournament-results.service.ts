d3a8fe2d470f6036e2a32a5fbdc8f2220c7d60ce
Jibaru <ignacioruedaboada@hotmail.com>
2022-07-23 09:38:42 -0500

    Added create, get and update tournament result service

--- src/services/tournament-results.service.ts
+++ src/services/tournament-results.service.ts
@@ -1,11 +1,16 @@
 import {
   API_DOMAIN,
+  CREATE_TOURNAMENT_RESULT,
   FETCH_TOURNAMENT_RESULTS,
+  GET_TOURNAMENT_RESULT,
+  UPDATE_TOURNAMENT_RESULT,
 } from "@/config/services-uri.config";
 import { TournamentResultJson } from "@/models/jsons/tournament-result.json";
 import { TournamentResult } from "@/models/tournament-result.model";
 import { AxiosError, AxiosInstance } from "axios";
+import { BasicResponse } from "./interfaces/basic-response";
 import { PaginatedResponse } from "./interfaces/paginated-response";
+import { ResourcedResponse } from "./interfaces/resourced-response";
 import { ResponseError } from "./interfaces/response-error";
 
 export interface TournamentResultsService {
@@ -15,6 +20,29 @@ export interface TournamentResultsService {
     pageSize?: number,
     token?: string
   ): Promise<PaginatedResponse<TournamentResult>>;
+  createTournamentResult(
+    tournamentId: string,
+    attributes: {
+      score: number;
+      place: number;
+      competitorId: string;
+    },
+    token: string
+  ): Promise<BasicResponse>;
+  getTournamentResult(
+    tournamentResultId: string,
+    token: string
+  ): Promise<ResourcedResponse<TournamentResult>>;
+  updateTournamentResult(
+    tournamentResultId: string,
+    tournamentId: string,
+    attributes: {
+      score?: number;
+      place?: number;
+      competitorId?: string;
+    },
+    token: string
+  ): Promise<BasicResponse>;
 }
 
 export class ApiTournamentResultsService implements TournamentResultsService {
@@ -55,6 +83,104 @@ export class ApiTournamentResultsService implements TournamentResultsService {
         response.data?.per_page,
         response.data?.total_pages
       );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(
+        error.response.data.message,
+        error.response.data.errors
+      );
+    }
+  }
+
+  async createTournamentResult(
+    tournamentId: string,
+    attributes: {
+      score: number;
+      place: number;
+      competitorId: string;
+    },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.post(
+        API_DOMAIN +
+          CREATE_TOURNAMENT_RESULT.replace(":tournamentId", tournamentId),
+        {
+          score: attributes.score,
+          place: attributes.place,
+          competitor_id: attributes.competitorId,
+        },
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(
+        error.response.data.message,
+        error.response.data.errors
+      );
+    }
+  }
+
+  async getTournamentResult(
+    tournamentResultId: string,
+    token: string
+  ): Promise<ResourcedResponse<TournamentResult>> {
+    try {
+      const response = await this._httpClient.get(
+        API_DOMAIN +
+          GET_TOURNAMENT_RESULT.replace(
+            ":tournamentResultId",
+            tournamentResultId
+          ),
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new ResourcedResponse(
+        response.data?.message,
+        TournamentResult.fromJson(response.data?.tournament_result)
+      );
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(
+        error.response.data.message,
+        error.response.data.errors
+      );
+    }
+  }
+
+  async updateTournamentResult(
+    tournamentResultId: string,
+    tournamentId: string,
+    attributes: {
+      score?: number;
+      place?: number;
+      competitorId: string;
+    },
+    token: string
+  ): Promise<BasicResponse> {
+    try {
+      const response = await this._httpClient.patch(
+        API_DOMAIN +
+          UPDATE_TOURNAMENT_RESULT.replace(
+            ":tournamentResultId",
+            tournamentResultId
+          ).replace(":tournamentId", tournamentId),
+        attributes,
+        {
+          headers: {
+            ...this._buildTokenHeader(token),
+          },
+        }
+      );
+
+      return new BasicResponse(response.data?.message);
     } catch (error: any | Error | AxiosError) {
       throw new ResponseError(
         error.response.data.message,
